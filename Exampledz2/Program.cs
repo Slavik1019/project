685db1aa5b793fe510061ceb79250a063498107d
Slava <slavik_1019@mail.ru>
2022-12-05 23:15:20 +0300

    ДЗ задача 2

---
+++ Exampledz2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b) Console.WriteLine("Первое число большее, Второе число меньшее");
+if (a < b) Console.WriteLine("Первое число меньшее, Второе число большее");
+if (a == b) Console.WriteLine("Числа равны");
