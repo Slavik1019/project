aeae2db4d9577090d87fb6ef8e9fec53fb968757
Slava <slavik_1019@mail.ru>
2022-12-06 00:19:49 +0300

    ДЗ задача 6

---
+++ Exampledz6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int i;
+i = int.Parse(Console.ReadLine());
+if (i % 2 == 0)
+{
+    Console.Write("Введенное число является четным");
+}
+else
+{
+    Console.Write("Введенное число является нечетное");
+}
