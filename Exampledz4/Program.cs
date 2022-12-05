e79721fea4cd049b60e81e52f5df988894508f89
Slava <slavik_1019@mail.ru>
2022-12-05 23:56:55 +0300

    ДЗ Задача 4

---
+++ Exampledz4/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите первое число:");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int Num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int Num3 = Convert.ToInt32(Console.ReadLine());
+int max;
+
+if (Num1 >= Num2)
+ {
+    if (Num3 >= Num1)
+    {
+       Console.WriteLine("Максимальное число: " + Num3);
+    }
+    else
+    {
+       Console.WriteLine("Максимальное число: " + Num1);
+    }
+ }
+ else
+ {
+    if (Num3 >= Num2)
+    {
+       Console.WriteLine("Максимальное число: " + Num3);
+    }
+    else
+    {
+       Console.WriteLine("Максимальное число: " + Num2);
+    }
+ }
+
+    
+ 
