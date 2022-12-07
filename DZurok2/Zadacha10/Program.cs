f93b15c7c573226128d2c2dffa01ae27b09b6503
Slava <slavik_1019@mail.ru>
2022-12-07 21:17:50 +0300

    Задача 10

---
+++ DZurok2/Zadacha10/Program.cs
@@ -0,0 +1,21 @@
+﻿int number = ReadInt("Введите трехзначное число: ");
+int count = number.ToString().Length;
+if (count < 3 || count > 3)
+{
+ Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+ Console.WriteLine(InCenter(number));
+}
+int ReadInt(string message)
+{
+ Console.Write(message);
+ return Convert.ToInt32(Console.ReadLine());
+}
+int InCenter(int a)
+{
+    
+ int result = ((a / 10) % 10);
+ return result;
+}
