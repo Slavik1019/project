a808303c4f6205d6c3117be106d17df487f8e175
Slava <slavik_1019@mail.ru>
2022-12-08 21:07:08 +0300

    Задача 15

---
+++ DZurok2/Zadacha15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 6)
+{
+    Console.WriteLine ("не является выходным");
+}
+if ((num == 6) || (num == 7))
+{
+    Console.WriteLine("является выходным");
+}
+if (num > 7)
+{
+    Console.WriteLine("Введенное число не соотвествует дням недели");
+}
