013958ae30ff7e52f08c9779b98c3e7c13cf2b8c
Slava <slavik_1019@mail.ru>
2022-12-12 22:47:55 +0300

    Задача 19

---
+++ DZurok3/Zadacha19/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine() ?? "";
+int num = number.Length;
+
+if (num == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+{
+Console.WriteLine($"число: {number} - является палиндромом");    
+}
+else
+{
+Console.WriteLine($"число: {number} - не является палиндром");
+   }
+}
+else
+{
+    Console.WriteLine($"Ошибка: число {number} - не являетя пятизначным");
+}
+        
+   
