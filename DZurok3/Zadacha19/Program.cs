abb2b942eaa15319b83f12361dae92c0de5de73e
Slava <slavik_1019@mail.ru>
2022-12-12 22:50:34 +0300

    задача 19

--- DZurok3/Zadacha19/Program.cs
+++ DZurok3/Zadacha19/Program.cs
@@ -5,11 +5,11 @@ int num = number.Length;
 if (num == 5)
 {
     if (number[0] == number[4] && number[1] == number[3])
-{
+   {
 Console.WriteLine($"число: {number} - является палиндромом");    
-}
+   }
 else
-{
+   {
 Console.WriteLine($"число: {number} - не является палиндром");
    }
 }
