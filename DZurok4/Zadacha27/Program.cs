9415d2889d0c3ac8fe3ad9a5235988d05040688f
Slava <slavik_1019@mail.ru>
2022-12-15 19:39:09 +0300

    Задача 27

---
+++ DZurok4/Zadacha27/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (n > 0)
+{
+int num = n % 10;
+n = n / 10;
+sum = sum + num;
+}
+Console.WriteLine($"сумма цифр в числе:{sum}");
