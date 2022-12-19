faa932d1a3a77cb982c38724537d11647923c395
Slava <slavik_1019@mail.ru>
2022-12-19 20:52:18 +0300

    Задача 34

---
+++ DZurok5/Zadcha34/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] num = new int[size];
+FillArrayRandomNumbers(num);
+Console.WriteLine("задан массив: ");
+PrintArray(num);
+int count = 0;
+
+for (int z = 0; z < num.Length; z++)
+if (num[z] % 2 == 0)
+count++;
+
+Console.WriteLine($"Найдено {count} четных числа");
+
+void FillArrayRandomNumbers(int[] num)
+{
+    for(int i = 0; i < num.Length; i++)
+    {
+        num[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] num)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < num.Length; i++)
+    {
+        Console.Write(num[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
